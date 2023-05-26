937289ef3dd407a6416dce502bd36dd2efb0e9f7
Hugo Vinícius <ugopessoa@gmail.com>
2023-05-26 15:32:42 -0300

    finaliznado o campo minado

--- campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -50,7 +50,7 @@ public class Campo {
         }
     }
 
-    void alternarMarcacao() {
+    public void alternarMarcacao() {
         if(!aberto) {
             marcado = !marcado;
             
@@ -62,7 +62,7 @@ public class Campo {
         }
     }
 
-    boolean abrir() {
+    public boolean abrir() {
 
         if(!aberto && !marcado) {
             if(minado) {
@@ -82,7 +82,7 @@ public class Campo {
         }
     }
 
-    boolean vizinhacaSegura() {
+    public boolean vizinhacaSegura() {
         return vizinhos.stream()
             .noneMatch(v -> v.minado);
     }
@@ -129,13 +129,14 @@ public class Campo {
         return desvendado || protegido;
     }
 
-    long minasNaVizinhanca() {
-        return vizinhos.stream().filter(v -> v.minado).count();
+    public int minasNaVizinhanca() {
+        return (int) vizinhos.stream().filter(v -> v.minado).count();
     }
 
     void reiniciar() {
         aberto = false;
         minado = false;
         marcado = false;
+        notificarObservadores(CampoEvento.REINICIAR);
     }
 }
--- campo-minado/src/br/com/cod3r/cm/visao/PainelTabuleiro.java
+++ campo-minado/src/br/com/cod3r/cm/visao/PainelTabuleiro.java
@@ -1,6 +1,10 @@
 package br.com.cod3r.cm.visao;
 
+import java.awt.GridLayout;
+
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import br.com.cod3r.cm.modelo.Tabuleiro;
 
@@ -9,5 +13,22 @@ public class PainelTabuleiro extends JPanel {
     
     public PainelTabuleiro(Tabuleiro tabuleiro) {
 
+        setLayout(new GridLayout(tabuleiro.getLinhas(), tabuleiro.getColunas()));
+
+        tabuleiro.paraCadaCampo(c -> add(new BotaoCampo(c)));
+
+        tabuleiro.registrarObservador(e -> {
+            
+            SwingUtilities.invokeLater(() -> {             
+                if(e.isGanhou()) {
+                    JOptionPane.showMessageDialog(null, "Ganhou :)");
+                } else {
+                    JOptionPane.showMessageDialog(null,"Perdeu :(");
+                }
+
+                tabuleiro.reiniciar();
+            });
+            
+        });
     }
 }
